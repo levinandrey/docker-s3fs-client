d66f847668f9d5729e735887c9147177ba7fba83
levinandrey <letlevin@gmail.com>
2021-11-02 01:12:27 +0300

    Update docker-entrypoint.sh
    UPD EXEC_S3FS_CODE

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -70,12 +70,16 @@ fi
 # sub-directory, so we can use the presence of some file/dir as a marker to
 # detect that mounting was a success. Execute the command on success.
 
-su - $RUN_AS -c "s3fs $DEBUG_OPTS ${S3FS_ARGS} \
+
+EXEC_S3FS_CODE="su - ${RUN_AS} -c s3fs $DEBUG_OPTS ${S3FS_ARGS} \
     -o passwd_file=${AWS_S3_AUTHFILE} \
     -o url=${AWS_S3_URL} \
     -o uid=$UID \
     -o gid=$GID \
     ${AWS_S3_BUCKET} ${AWS_S3_MOUNT}"
+
+echo $EXEC_S3FS_CODE
+$EXEC_S3FS_CODE    
 
 # s3fs can claim to have a mount even though it didn't succeed.
 # Doing an operation actually forces it to detect that and remove the mount.
